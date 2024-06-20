98dba97f7ad3c473d098f826b507b505ca89f47a
Alexis Martinez <jhonyalexis100@gmail.com>
2024-06-20 18:08:12 -0500

    build entity book

---
+++ rest-api-library/src/main/java/jagarcia/rest_api_library/entity/Book.java
@@ -0,0 +1,31 @@
+package jagarcia.rest_api_library.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "books")
+public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false)
+    private String title;
+    @Column(nullable = false)
+    private String author;
+    @Column(nullable = false)
+    private String category;
+    @Column(nullable = false)
+    private String isbn;
+    @Column(nullable = false)
+    private Integer numberPages;
+    private Integer copiesAvailable;
+
+}
---
+++ rest-api-library/src/main/java/jagarcia/rest_api_library/repository/IBookRepository.java
@@ -0,0 +1,8 @@
+package jagarcia.rest_api_library.repository;
+
+import jagarcia.rest_api_library.entity.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface IBookRepository extends JpaRepository<Long, Book> {
+
+}
