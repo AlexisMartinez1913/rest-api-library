39fa8ad8ae41d59b5d5c8958ec0ff92a257094c5
Alexis Martinez <jhonyalexis100@gmail.com>
2024-06-20 17:56:40 -0500

    init project

---
+++ rest-api-library/src/test/java/jagarcia/rest_api_library/RestApiLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package jagarcia.rest_api_library;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiLibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
